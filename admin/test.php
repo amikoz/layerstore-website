051e8561fdf852e66735784b90fe4877249a82b1
Kleemann <j.c.kleemann@web.de>
2026-02-27 21:12:35 +0100

    chore: remove unused PHP files

--- admin/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'status' => 'OK',
-    'php_version' => phpversion(),
-    'request_method' => $_SERVER['REQUEST_METHOD'],
-    'post' => $_POST,
-    'input' => file_get_contents('php://input')
-]);
